96f534eaa4b24c293d5e628558b12ee8b0c3313b
Alberto Fernandez <infjaf@gmail.com>
2020-04-09 21:43:59 +0200

    Release 4.6.10.ayg

--- components/rich/src/test/integration/org/richfaces/integration/partialViewContext/ITStatelessViews.java
+++ components/rich/src/test/integration/org/richfaces/integration/partialViewContext/ITStatelessViews.java
@@ -47,7 +47,7 @@ public class ITStatelessViews {
     public static WebArchive createDeployment() {
         RichDeployment deployment = new RichDeployment(ITStatelessViews.class);
 
-        deployment.addMavenDependency("com.github.albfernandez.richfaces:richfaces-a4j:4.6.10-SNAPSHOT");
+        deployment.addMavenDependency("com.github.albfernandez.richfaces:richfaces-a4j:4.6.10.ayg");
 
         addIndexPage(deployment);
 
