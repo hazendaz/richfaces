40ab294d362b3bc817ecf51b3e1b782bb74c115e
Alberto Fernandez <infjaf@gmail.com>
2020-04-16 01:19:31 +0200

    Release 4.6.12.ayg

--- core/src/test/integration/org/richfaces/deployment/CoreDeployment.java
+++ core/src/test/integration/org/richfaces/deployment/CoreDeployment.java
@@ -447,7 +447,7 @@ public class CoreDeployment extends BaseDeployment {
     }
     
     public void withA4jComponents() {
-        addMavenDependency("com.github.albfernandez.richfaces:richfaces-a4j:4.6.12-SNAPSHOT");
+        addMavenDependency("com.github.albfernandez.richfaces:richfaces-a4j:4.6.12.ayg");
         excludeMavenDependency("richfaces-core");
     }
     
